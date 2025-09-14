d34667412dc8a240988779cecf2279d36e8365be
Shivam-sah304 <shivma_081bct039@tcioe.edu.np>
2025-09-14 07:41:48 +0545

    Doing operation on 1D list

---
+++ calc1.py
@@ -0,0 +1,29 @@
+# Do all the operation with a list
+list=[2,3,5,6,7,8,9]
+#add one element
+list.append(10)
+print(list)
+#find the 2-5 index from list
+print(list[1:5])
+#Find the maximum value of list
+print(max(list))
+#find the reverse order of list
+list.reverse()
+print(list)
+#remove 5 from the list
+list.reverse()
+list.remove(5)
+print(list)
+#add one string value in list 
+list.append("shivam")
+print(list)
+#adding a nwe list in the list
+list1=["shivam","shishir","rajan","samir"]
+new_list=list+list1
+print(new_list)
+#removing one shivam from the list
+new_list.remove("shivam")
+print(new_list)
+#find the length of new list
+print(len(new_list))
+
