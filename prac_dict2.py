063a92449d056456d623fe65e1155ab3c6079520
Shivam-sah304 <shivma_081bct039@tcioe.edu.np>
2025-09-15 13:22:16 +0545

    Updating and delating of dictionary keys

---
+++ prac_dict2.py
@@ -0,0 +1,9 @@
+#Given the dictionary:
+
+
+person = {"name": "Aditi", "city": "Delhi", "age": 25}
+#Change the "city" value to "Mumbai".
+person["city"]="Mumbai"
+#Delete the key "age".
+del(person["age"])
+print(person)
